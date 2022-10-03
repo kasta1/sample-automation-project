4be9445166b0d12e3d2f3e9ee484b28fe1ec0e03
Anastasia Klachun <aklachun77@gmail.com>
2022-10-03 14:12:25 +0300

    idea push

--- src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
+++ src/test/java/net/absoft/mortalwombat/AgeCheckerTest.java
@@ -8,12 +8,21 @@ public class AgeCheckerTest {
     @Test
     public void testAgedUserCanPlay(){
         AgeChecker ageChecker = new AgeChecker();
-        Assert.assertTrue(ageChecker.canPlayGame(19),"aged user cant play game");
+        Assert.assertTrue(ageChecker.canPlayGame(21),"aged user cant play game");
 
     }
 @Test
     public void  testThatTooYongUsersCanNotPlay(){
         AgeChecker ageChecker = new AgeChecker();
-        Assert.assertTrue(ageChecker.canPlayGame(21),"not aged user can't play game");
+        Assert.assertTrue(ageChecker.canPlayGame(19),"under aged user can't play game");
     }
+
+    @Test
+    public void testUnderAgedUserCanPlay(){
+        AgeChecker ageChecker = new AgeChecker();
+        Assert.assertFalse(ageChecker.canPlayGame(17),"Under aged user  play game");
+
+    }
+
 }
+
